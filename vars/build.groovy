9097ad5bb22e6f156965a19d31baa070f2dc2c77
chandana48 <51244331+chandana48@users.noreply.github.com>
2021-07-20 16:39:18 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -21,8 +21,7 @@ def call(String repoUrl) {
         }
         stage('package publisher'){
             steps{
-                nexusArtifactUploader credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT'
-            }
+                nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: 'C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\casestudy-1\\target\\my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT' }
         }
        }
     post {
