26c01ce82ee93b9e7ecf6ea33756268dc93765e3
chandana48 <51244331+chandana48@users.noreply.github.com>
2021-07-20 16:53:56 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,13 +4,13 @@ def call(String repoUrl) {
        
        stages {
            
-           stage("Checkout Code") {
+           stage("git") {
                steps {
                    git branch: 'master',
                        url: "${repoUrl}"
                }
            }
-           stage('Build') {
+           stage('build') {
             steps {
                 script {
                     def mvnhome = tool name: 'jenkins-maven', type: 'maven'
@@ -19,7 +19,7 @@ def call(String repoUrl) {
             }
 
         }
-        stage('package publisher'){
+        stage('deploy'){
             steps{
                 nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: 'C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\casestudy-1\\target\\my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT' }
         }
@@ -35,7 +35,7 @@ failure {
 success{
     script{
     if (currentBuild.currentResult == 'SUCCESS') {
-      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "rachhachandana48@gmail.com", sendToIndividuals: true])
+      step([$class: 'Mailer', notifyEveryUnstableBuild: false, recipients: "rachhachandana48@gmail.com", sendToIndividuals: true])
     }
     }
 }
