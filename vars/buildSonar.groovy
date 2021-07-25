0d2a11d898811dcae4b5e650e921c0c3db20f1b1
chandana48 <51244331+chandana48@users.noreply.github.com>
2021-07-25 22:07:06 +0530

    Update buildSonar.groovy

--- vars/buildSonar.groovy
+++ vars/buildSonar.groovy
@@ -28,6 +28,15 @@ def call(String repoUrl) {
                   }
               }
             }
+         
+         stage("Quality Gate Status Check"){
+          timeout(time: 1, unit: 'HOURS') {
+              def qg = waitForQualityGate()
+              if (qg.status != 'OK') {
+                mail bcc: '', body: 'Sonar Quality gate failed and build cannot be proceeded', cc: '', from: '', replyTo: '', subject: 'Sonar Failed', to: 'rachhachandana48@gmail.com'
+              }
+          }
+         }
         stage ('Server'){
                         steps {
                            rtServer (
