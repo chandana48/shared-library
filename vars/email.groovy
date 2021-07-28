b36bebbd30783998d7690f279f5322cc38e4a4c3
chandana48 <51244331+chandana48@users.noreply.github.com>
2021-07-28 18:23:03 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,4 +1,4 @@
-def call(Map config) {
+def call(Map config = [:]) {
    
    mail bcc: '', body: '${config.body}', cc: '', from: '', replyTo: '', subject: '${config.subject}', to: '${config.to}'
    
