9ffb2e35cbb7454d6259d970c4da09c0e97ef71e
chandana48 <51244331+chandana48@users.noreply.github.com>
2021-08-02 17:07:14 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,10 +1,8 @@
 import org.Constants.groovy;
 
 def call(String action){
-  steps{
       script {
               def mvnhome = tool "${MAVEN}";
               bat "${mvnhome}/bin/mvn ${action}"
          }
-  }
 }
